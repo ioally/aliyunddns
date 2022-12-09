4bfaff0d92ae318503045a41e6fabb76df4ce07e
heweidong <cloudtry.hwd@gmail.com>
2022-12-09 20:20:40 +0800

    git update: 更换为亚马逊的ip查询api

--- src/main/java/com/cloutry/aliyunddns/api/RemoteQueryIpApi.java
+++ src/main/java/com/cloutry/aliyunddns/api/RemoteQueryIpApi.java
@@ -5,7 +5,9 @@ import feign.RequestLine;
 
 public interface RemoteQueryIpApi {
 
+    String USER_AGENT = "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36;";
+
     @RequestLine("GET /")
-    @Headers(QueryIpUrlApi.USER_AGENT)
+    @Headers(USER_AGENT)
     byte[] queryIpByRemote();
 }
